26263a5f4776b45ceb127789efb77d47413e77f3
tyrone-yanaga <tkyanaga@gmail.com>
2024-04-17 22:25:47 -0700

    map marker class created

--- BipMap/models.py
+++ BipMap/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Map_marker(models.Model):
+class MapMarker(models.Model):
     crimetype = models.CharField(max_length=200)
     datetime = models.CharField(max_length=200)
     casenumber = models.CharField(max_length=200)
@@ -9,4 +9,5 @@ class Map_marker(models.Model):
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
     state = models.CharField(max_length=200)
-    location = models.ArrayField(models.CharField(max_length=200), size=2,)
+    lat = models.DecimalField()
+    lon = models.DecimalField()
